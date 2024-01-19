1fe2f7c4889bcfaf7da259d993e15ddb3b0a895d
clcl777 <77223796+clcl777@users.noreply.github.com>
2024-01-19 21:03:22 +0900

    add user controller

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
-end
+
+    # POST /users
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        render json: @user, status: :created, location: @user
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # GET /users/:id
+    def show
+      @user = User.find(params[:id])
+      render json: @user
+    end
+  
+    private
+    def user_params
+      params.require(:user).permit(:name, :email, :google_id)
+    end
+  end
+  
