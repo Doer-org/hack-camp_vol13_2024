65b44b684f871270661eb4c02f085eb6a3ba0aca
clcl777 <77223796+clcl777@users.noreply.github.com>
2024-01-19 18:54:50 +0900

    update routes

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:create, :show] do
+    resources :projects, only: [:create, :update, :destroy, :index, :show]
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :projects, only: [] do
+    resources :formulas, only: [:create, :update, :destroy, :index]
+  end
 end
