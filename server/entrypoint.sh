ca3dee4c0f1d7effcdd29412a762875fcbb19362
clcl777 <77223796+clcl777@users.noreply.github.com>
2024-01-20 16:42:24 +0900

    update db info

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -8,6 +8,6 @@ rm -f /myapp/tmp/pids/server.pid
 bundle install
 # bundle exec rake assets:precompile
 # bundle exec rake assets:clean
-bundle exec rake db:migrate
+# bundle exec rake db:migrate
 
 exec "$@"
