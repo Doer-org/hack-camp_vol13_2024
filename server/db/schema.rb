95adbd2ab94af227e0b0252479233e91038dfe09
clcl777 <77223796+clcl777@users.noreply.github.com>
2024-01-31 21:46:23 +0900

    image_urlカラム追加

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_31_124035) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_19_140837) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,7 +48,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_31_124035) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "image_url"
     t.index ["project_id"], name: "index_formulas_on_project_id"
   end
 
